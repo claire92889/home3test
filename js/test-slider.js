b407786ef52320fb0493ba614f1651287003a90e
claire92889 <77464977+claire92889@users.noreply.github.com>
2021-01-25 19:45:17 +0800

    Add files via upload

---
+++ js/test-slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+$('.testimony-slider').slick({
+	slidesToShow: 2,
+	slidesToScroll: 1,
+	dots: true,
+	autoplay:true,
+	autoplayspeed:5000, 
+});
+});
+
+$(document).ready(function() {
+$('.affil-logos').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplay:true,
+	autoplayspeed:5000,
+	nextArrow:false,
+	prevArrow:false,
+});
+});
